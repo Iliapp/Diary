def7021e3cce52ff0b5204d2ea10f98e5c2f4068
Ilya Smakovskyi <iluhasmakovskyi@gmail.com>
2025-09-08 16:06:57 +0200

    Added support for tags<labels> for records: tags and entries_tags tables

---
+++ sql/tags.sql
@@ -0,0 +1,6 @@
+CREATE TABLE tags (
+                      id UUID PRIMARY KEY DEFAULT  gen_random_uuid(),
+                      name TEXT NOT NULL,
+                      created_at TIMESTAMP DEFAULT now()
+
+);
