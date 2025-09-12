b2bd08a59436791a2f4b9a42e5e3af739e5cf699
Ilya Smakovskyi <iluhasmakovskyi@gmail.com>
2025-09-12 15:28:57 +0200

    Add repository(EntryRepository) this file works with the database via JPA

---
+++ backend/src/main/java/repository/EntryRepository.java
@@ -0,0 +1,22 @@
+package repository;
+
+import com.iliasmakovskiy.diary.model.Entry;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import java.util.List;
+import java.util.UUID;
+
+
+public interface EntryRepository  extends JpaRepository<Entry,UUID> {
+
+
+
+    List<Entry> findByUserId(UUID userId);
+
+
+
+
+
+}
+
