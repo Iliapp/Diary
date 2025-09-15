92529e138080d4d5a247c5e143a81a550d673ad3
Ilya Smakovskyi <iluhasmakovskyi@gmail.com>
2025-09-15 13:41:16 +0200

    add migration( V4__create_entries_tags_table.sql)

--- backend/src/main/resources/db/migration/V3__create_entries_table.sql
+++ backend/src/main/resources/db/migration/V3__create_entries_table.sql
@@ -13,3 +13,6 @@ CREATE TABLE entries (
 
 
 );
+
+
+
---
+++ backend/src/main/resources/db/migration/V4__create_entries_tags_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE entries_tags (
+                              id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                              entry_id UUID REFERENCES entries(id) ON DELETE CASCADE,
+                              tag_id UUID REFERENCES tags(id) ON DELETE CASCADE,
+                              created_at TIMESTAMP DEFAULT now()
+);
