031fd3f7637bc3094af6cd909aa6eb6549005e18
Ilya Smakovskyi <iluhasmakovskyi@gmail.com>
2025-09-15 11:11:04 +0200

    Add in module contoller(EntryController) GetMapping

--- backend/src/main/java/com/ilya/backend/contoller/EntryController.java
+++ backend/src/main/java/com/ilya/backend/contoller/EntryController.java
@@ -2,6 +2,32 @@ package com.ilya.backend.contoller;
 
 
 
+import com.ilya.backend.model.Entry;
+import com.ilya.backend.service.EntryService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/entries")
+
 public class EntryController {
+
+    private final EntryService entryService;
+
+    public EntryController(EntryService entryService) {
+        this.entryService = entryService;
+    }
+    //Get all elements
+    @GetMapping
+    public List<Entry> getAllEntries() {
+        return entryService.getAllEntry();
+    }
+
+
+
+
 
 }
